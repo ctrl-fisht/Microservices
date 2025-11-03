87ce5d06df531c058e0279f7b87e35bcda4b3448
ctrlfisht <ctrlshiftweb@gmail.com>
2025-11-03 14:48:59 +0700

    return seq back

--- DirectoryService/src/DirectoryService.WebAPI/Extensions/LoggingExtensions.cs
+++ DirectoryService/src/DirectoryService.WebAPI/Extensions/LoggingExtensions.cs
@@ -6,12 +6,17 @@ public static class LoggingExtensions
 {
     public static void AddLogging(this ConfigureHostBuilder host, IConfiguration configuration)
     {
+        var seqConnString = configuration.GetConnectionString("Seq");
+        if  (string.IsNullOrEmpty(seqConnString))
+            throw new Exception("Seq connection string not found");
+        
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Information()                  
             .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
             .Enrich.FromLogContext()       
             .Enrich.WithProperty("ServiceName", "DirectoryService")
-            .WriteTo.Console()                                
+            .WriteTo.Console()                        
+            .WriteTo.Seq(seqConnString)
             .CreateLogger();
 
         host.UseSerilog();
--- FileService/src/FileService.Core/Extensions/LoggingExtensions.cs
+++ FileService/src/FileService.Core/Extensions/LoggingExtensions.cs
@@ -6,11 +6,16 @@ public static class LoggingExtensions
 {
     public static void AddLogging(this ConfigureHostBuilder host, IConfiguration configuration)
     {
+        var seqConnString = configuration.GetConnectionString("Seq");
+        if  (string.IsNullOrEmpty(seqConnString))
+            throw new Exception("Seq connection string not found");
+        
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Information()                       
             .MinimumLevel.Override("Microsoft", Serilog.Events.LogEventLevel.Information)
             .Enrich.FromLogContext()
             .Enrich.WithProperty("ServiceName", "FileService")
+            .WriteTo.Seq(seqConnString)
             .WriteTo.Console()                                
             .CreateLogger();
 
